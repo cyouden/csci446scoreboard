1aa489114b2d90cc39ea96c8e495d6ffaba49082
Christopher Youden <cyouden@mines.edu>
2012-05-08 12:22:15 -0600

    adding project 8 initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scoreboard::Application.config.secret_token = '60a77cf67d45955d1b6c25ca30e97a84a0f820dcaccda762be06c3bf191b5925d7f7b80b5f2eed5450923d0252baf64699d9258105dd52ef9605da8caa9d1510'
